9628e9230efc5962c1437de673967f4aa522222c
Farhad <farhad.hossain@cefalo.com>
2025-05-17 02:45:59 +0600

    Refactor code

--- client/app/negotiation-tips/page.tsx
+++ client/app/negotiation-tips/page.tsx
@@ -1,9 +1,9 @@
-import { NegotiationTips } from "@/components/negotiation-tips"
+import {NegotiationTips} from "@/components/negotiation-tips"
 
 export default function NegotiationTipsPage() {
-  return (
-    <div className="container mx-auto py-6">
-      <NegotiationTips />
-    </div>
-  )
+    return (
+        <div className="container mx-auto py-6">
+            <NegotiationTips/>
+        </div>
+    )
 }
